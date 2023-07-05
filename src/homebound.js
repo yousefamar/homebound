0cbd1c04060c3294912642982afdd3b8cc4d5fef
Yousef Amar <yousefamar@gmail.com>
2023-07-05 19:18:55 +0100

    add: top margin back only if not in iframe

--- src/homebound.js
+++ src/homebound.js
@@ -46,6 +46,9 @@ var HB = {
 
 		var canvas = document.getElementById('canvas');
 
+		if (window.location === window.parent.location)
+			canvas.style.marginTop = '100px';
+
 		const TICK_INTERVAL_MS = 1000.0/100.0;
 
 		var lastTick = Date.now();
